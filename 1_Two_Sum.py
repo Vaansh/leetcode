f0ba5357651293123307a75395b557631f996485
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 14:17:32 -0400

    Delete 1_Two_Sum.py

--- 1_Two_Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            c = target - n
-            if c in d:
-                return [d[c], i]
-            d[n] = i
