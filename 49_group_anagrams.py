2b4ae154ca13670fb5c0e72f2a88d1284e5a3b8f
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 14:18:31 -0400

    Create 49_group_anagrams.py

---
+++ 49_group_anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+
+        for st in strs:
+            s = "".join([x for x in sorted(st)])
+            temp = d.get(s, [])
+            temp.append(st)
+            d[s] = temp
+        
+        return d.values()
