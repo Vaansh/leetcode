14ddaeb3ce0308f674efc1ee37c12d0933c5326c
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 21:04:15 -0400

    updated solution for problem 0125

--- 0125-valid-palindrome.py
+++ 0125-valid-palindrome.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        tmp = "".join(c.lower() for c in s if c.isalnum())
-        return tmp == tmp[::-1]
+        res = "".join(c.lower() for c in s if c.isalnum())
+        return res == res[::-1]
