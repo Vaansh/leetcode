992b61a58cd90016eb16b5695203c2dc90ca08fa
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 21:04:15 -0400

    updated solution for problem 0167

--- 0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         d = {}
+
         for i, n in enumerate(numbers):
-            c = target - n
-            if c in d:
-                return [d[c] + 1, i + 1]
+            complement = target - n
+            if complement in d:
+                return [d[complement] + 1, i + 1]
             d[n] = i
