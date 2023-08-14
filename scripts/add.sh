6baf93f6f6e12c0a37d735daab78114b150808eb
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 20:43:15 -0400

    updated file scripts/add.sh

--- scripts/add.sh
+++ scripts/add.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
 
+. ./scripts/count.sh
+. ./scripts/rename.sh
+
+rename "."
+
 # Add all changes, staged or unstaged
 git add -A
 
-# Get the list of added and modified files
+# Get the list of added, modified, and deleted files
 added_files=$(git diff --cached --name-only --diff-filter=A)
 modified_files=$(git diff --cached --name-only --diff-filter=M)
+deleted_files=$(git diff --cached --name-only --diff-filter=D)
 
 # Iterate over added files
 for file in $added_files; do
-    if [[ $file =~ ^[0-9]+.*\.py$ ]]; then
-        if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
-            problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
-            git commit -m "added solution for problem $problem_number" "$file"
-        else
-            git commit -m "added file $file" "$file"
-            echo "a"
-        fi
+    if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
+        problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
+        git commit -m "added solution for problem $problem_number" "$file"
+    else
+        git commit -m "added file $file" "$file"
     fi
 done
 
 # Iterate over modified files
 for file in $modified_files; do
-    if [[ $file =~ ^[0-9]+.*\.py$ ]]; then
-        if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
-            problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
-            git commit -m "updated solution for problem $problem_number" "$file"
-        else
-            git commit -m "updated file $file" "$file"
-            echo "b"
-        fi
+    if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
+        problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
+        git commit -m "updated solution for problem $problem_number" "$file"
+    else
+        git commit -m "updated file $file" "$file"
+    fi
+done
+
+# Iterate over deleted files
+for file in $deleted_files; do
+    if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
+        problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
+        git commit -m "deleted solution for problem $problem_number" "$file"
+    else
+        git commit -m "deleted file $file" "$file"
     fi
 done
 
