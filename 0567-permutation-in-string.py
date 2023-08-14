45e83c81d507817671669571e9f15fa4d56d32fb
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 21:06:26 -0400

    updated solution for problem 0567

--- 0567-permutation-in-string.py
+++ 0567-permutation-in-string.py
@@ -4,11 +4,10 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n, m = len(s1), len(s2)
         s1_freq, s2_freq = Counter(s1), Counter(s2[:n])
-        
+
         for i in range(n, m):
             if s1_freq == s2_freq:
                 return True
-
             s2_freq[s2[i - n]] -= 1
             s2_freq[s2[i]] += 1
         
