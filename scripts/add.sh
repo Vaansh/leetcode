48abdf68b9ee4b045ba83f69001065bb1ec54c60
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 20:47:04 -0400

    updated file add.sh

--- scripts/add.sh
+++ scripts/add.sh
@@ -16,30 +16,36 @@ deleted_files=$(git diff --cached --name-only --diff-filter=D)
 # Iterate over added files
 for file in $added_files; do
     if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
-        problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
+        file_name=$(basename "$file")
+        problem_number=$(echo "$file_name" | grep -oE '[0-9]+' | head -n 1)
         git commit -m "added solution for problem $problem_number" "$file"
     else
-        git commit -m "added file $file" "$file"
+        file_name=$(basename "$file")
+        git commit -m "added file $file_name" "$file"
     fi
 done
 
 # Iterate over modified files
 for file in $modified_files; do
     if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
-        problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
+        file_name=$(basename "$file")
+        problem_number=$(echo "$file_name" | grep -oE '[0-9]+' | head -n 1)
         git commit -m "updated solution for problem $problem_number" "$file"
     else
-        git commit -m "updated file $file" "$file"
+        file_name=$(basename "$file")
+        git commit -m "updated file $file_name" "$file"
     fi
 done
 
 # Iterate over deleted files
 for file in $deleted_files; do
     if [[ $file =~ ^[0-9]+-.*\.py$ ]]; then
-        problem_number=$(echo "$file" | grep -oE '[0-9]+' | head -n 1)
+        file_name=$(basename "$file")
+        problem_number=$(echo "$file_name" | grep -oE '[0-9]+' | head -n 1)
         git commit -m "deleted solution for problem $problem_number" "$file"
     else
-        git commit -m "deleted file $file" "$file"
+        file_name=$(basename "$file")
+        git commit -m "deleted file $file_name" "$file"
     fi
 done
 
