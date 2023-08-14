052327a4fda74ff2beadafeaf44d38912180f7ea
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-13 21:10:38 -0400

    updated solution for problem 1512

--- 1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs.py
@@ -1,4 +1,3 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         return len([None for i in range(len(nums)) for j in range(i, len(nums)) if i < j and nums[i] == nums[j]])
-    
