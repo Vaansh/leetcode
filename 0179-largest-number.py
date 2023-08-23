58374ab9290a2bf8d17cf2dec8217780e8c9d89e
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-23 01:29:20 -0400

    updated solution for problem 0179

--- 0179-largest-number.py
+++ 0179-largest-number.py
@@ -1,11 +1,13 @@
 import functools
 
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         nums = list(map(str, nums))
+
         def compare(x, y):
             return int(y + x) - int(x + y)
-        
+
         nums.sort(key=functools.cmp_to_key(compare))
-        largest_num = ''.join(nums).lstrip('0')
-        return largest_num if largest_num else '0'
+        largest_num = "".join(nums).lstrip("0")
+        return largest_num if largest_num else "0"
