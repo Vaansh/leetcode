7d7469706e628daf2e546cf09d29800d9a135305
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-23 02:03:42 -0400

    updated solution for problem 1930

--- 1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences.py
@@ -1,10 +1,11 @@
 from collections import Counter
 
+
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         res = 0
         for c in set(s):
             a, b = s.find(c), s.rfind(c)
             if a < b:
-                res += len(set(s[a+1:b]))
+                res += len(set(s[a + 1 : b]))
         return res
