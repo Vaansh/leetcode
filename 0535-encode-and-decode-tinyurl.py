40074798331f996f314e2a90a3f16dbcb068bb50
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-22 22:22:31 -0400

    updated solution for problem 0535

--- 0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl.py
@@ -3,18 +3,17 @@ class Codec:
         self.urls = {}
 
     def encode(self, longUrl: str) -> str:
-        """Encodes a URL to a shortened URL.
-        """
+        """Encodes a URL to a shortened URL."""
         shortUrl = "https://tinyurl.com/" + str(hash(longUrl))
         self.urls[shortUrl] = longUrl
         return shortUrl
 
     def decode(self, shortUrl: str) -> str:
-        """Decodes a shortened URL to its original URL.
-        """
+        """Decodes a shortened URL to its original URL."""
         longUrl = self.urls[shortUrl]
         del self.urls[shortUrl]
         return longUrl
+
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
