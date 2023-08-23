900c61dcd4c3a101b7c99dca0afb28a1548f49a0
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-23 01:29:20 -0400

    updated solution for problem 1603

--- 1603-design-parking-system.py
+++ 1603-design-parking-system.py
@@ -1,15 +1,13 @@
 class ParkingSystem:
-
     def __init__(self, big: int, medium: int, small: int):
-        self.qty_map = {1 : big, 2 : medium, 3 : small}
-        self.parking_map = {1 : 0, 2 : 0, 3 : 0}
+        self.qty_map = {1: big, 2: medium, 3: small}
+        self.parking_map = {1: 0, 2: 0, 3: 0}
 
     def addCar(self, carType: int) -> bool:
         if self.parking_map[carType] < self.qty_map[carType]:
             self.parking_map[carType] += 1
             return True
         return False
-        
 
 
 # Your ParkingSystem object will be instantiated and called as such:
