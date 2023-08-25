885da2df953dc6e6a3bd594ee43ce136bb5710c7
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-25 18:30:30 -0400

    python: deleted solution for problem 0167

--- 0167-two-sum-ii-input-array-is-sorted.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        d = {}
-
-        for i, n in enumerate(numbers):
-            complement = target - n
-            if complement in d:
-                return [d[complement] + 1, i + 1]
-            d[n] = i
