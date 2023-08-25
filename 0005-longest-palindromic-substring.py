694bd0fdeac7e73e0e9604b9e40ee98c5bf30228
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-25 02:15:40 -0400

    updated solution for problem 0005

--- 0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring.py
@@ -1,13 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-
         def helper(l, r):
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
             return s[l + 1 : r]
 
+        res = ""
         for i in range(len(s)):
-            res = max(res, helper(i, i + 1), helper(i, i), key=len)
+            res = max(res, helper(i, i), helper(i, i + 1), key=len)
         return res
