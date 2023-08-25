b13420cb7ebac5f84da5ba415549804f2df67489
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-25 18:30:29 -0400

    python: deleted solution for problem 0078

--- 0078-subsets.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for n in nums:
-            res += [r + [n] for r in res]
-        return res
