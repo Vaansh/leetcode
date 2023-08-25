540d68386135ddd60c0c8bc3c304dbc77d508b3e
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-24 22:54:11 -0400

    updated solution for problem 0576

--- 0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths.py
@@ -22,29 +22,30 @@ class Solution:
         return helper(startRow, startColumn, 0) % mod
 
 
-class Solution:
-    def findPaths(
-        self, m: int, n: int, maxMove: int, startRow: int, startColumn: int
-    ) -> int:
-        mod = 10**9 + 7
-        memo = {}
+# TLE
+# class Solution:
+#     def findPaths(
+#         self, m: int, n: int, maxMove: int, startRow: int, startColumn: int
+#     ) -> int:
+#         mod = 10**9 + 7
+#         memo = {}
 
-        def helper(i, j, moves):
-            if not (0 <= i < m and 0 <= j < n):
-                return 1
+#         def helper(i, j, moves):
+#             if not (0 <= i < m and 0 <= j < n):
+#                 return 1
 
-            if moves == maxMove:
-                return 0
+#             if moves == maxMove:
+#                 return 0
 
-            if (i, j, moves) in memo:
-                return memo[(i, j, moves)] % mod
+#             if (i, j, moves) in memo:
+#                 return memo[(i, j, moves)] % mod
 
-            memo[(i, j, moves + 1)] = (
-                helper(i + 1, j, moves + 1)
-                + helper(i - 1, j, moves + 1)
-                + helper(i, j + 1, moves + 1)
-                + helper(i, j - 1, moves + 1)
-            )
-            return memo[(i, j, moves + 1)] % mod
+#             memo[(i, j, moves + 1)] = (
+#                 helper(i + 1, j, moves + 1)
+#                 + helper(i - 1, j, moves + 1)
+#                 + helper(i, j + 1, moves + 1)
+#                 + helper(i, j - 1, moves + 1)
+#             )
+#             return memo[(i, j, moves + 1)] % mod
 
-        return helper(startRow, startColumn, 0) % mod
+#         return helper(startRow, startColumn, 0) % mod
