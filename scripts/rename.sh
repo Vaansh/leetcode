42a9a245d86ede6b4c1420a3e2fddc9b4d288ced
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-25 17:37:08 -0400

    updated file rename.sh

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -7,7 +7,15 @@ rename() {
             if [ ! -z "$match" ]; then
                 number=$(echo "$match" | sed -E 's/([0-9]+)\. (.+)/\1/' | printf "%04d" "$(cat -)")
                 title=$(echo "$match" | sed -E 's/([0-9]+)\. (.+)/\2/' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g' | tr -d '.')
-                new_filename="${number}-${title}.py"
+                
+                # Check if the file is in the "java/" folder
+                if [[ "$filename" == *"java/"* ]]; then
+                    new_extension=".java"
+                else
+                    new_extension=".py"
+                fi
+                
+                new_filename="${number}-${title}${new_extension}"
                 mv "$filename" "$1/$new_filename"
                 echo "Renamed '$filename' to '$new_filename'"
             fi
