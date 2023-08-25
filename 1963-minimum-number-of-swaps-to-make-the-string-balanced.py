912041247c55fb8003c8864d17d5df625bd72d3e
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-25 18:30:32 -0400

    python: deleted solution for problem 1963

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minSwaps(self, s: str) -> int:
-        res = 0
-        for c in s:
-            if c == "[":
-                res += 1
-            elif res:
-                res -= 1
-        return (res + 1) // 2
