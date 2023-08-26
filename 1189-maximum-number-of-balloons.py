f7f9217dfa3019c75a09b521cacefd67abc53e5d
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-26 04:16:42 -0400

    python: updated solution for problem 1189

---
+++ 1189-maximum-number-of-balloons.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        return min(
+            text.count("b"),
+            text.count("a"),
+            text.count("l") // 2,
+            text.count("o") // 2,
+            text.count("n"),
+        )
+
+
+# from collections import Counter
+# class Solution:
+#     def maxNumberOfBalloons(self, text: str) -> int:
+#         counter_text = Counter(text)
+#         return min(
+#             counter_text.get("b", 0),
+#             counter_text.get("a", 0),
+#             counter_text.get("l", 0) // 2,
+#             counter_text.get("o", 0) // 2,
+#             counter_text.get("n", 0),
+#         )
