a9b8f3374a01c08fd18bd42f3cebf460da463e3d
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-26 04:16:39 -0400

    python: updated solution for problem 0020

---
+++ 0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        left_map = {"(": ")", "{": "}", "[": "]"}
+        right_map = {")": "(", "}": "{", "]": "["}
+
+        stack = []
+
+        for p in s:
+            if p in left_map:
+                stack.append(p)
+            elif p in right_map:
+                if len(stack) == 0:
+                    return False
+                elif left_map[stack.pop()] != p:
+                    return False
+
+        return len(stack) == 0
