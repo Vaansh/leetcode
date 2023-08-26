17c0ae1684eb88a75797d3a188b522ca25a16022
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-26 04:06:00 -0400

    java: added solution for problem 0198

---
+++ java/0198-house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> memo = new HashMap<>();
+        return helper(n, 0, nums, memo);
+    }
+
+    private int helper(int n, int i, int[] nums, Map<Integer, Integer> memo) {
+        if (i >= n) return 0;
+        if (memo.containsKey(i)) return memo.get(i);
+        int res = Math.max(helper(n, i + 1, nums, memo), helper(n, i + 2, nums, memo) + nums[i]);
+        memo.put(i, res);
+        return res;
+    }
+}
