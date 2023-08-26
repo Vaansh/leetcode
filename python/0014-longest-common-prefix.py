660517478b3ca97fb338c70986e907ed37df1c19
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-26 04:17:28 -0400

    python: deleted solution for problem 0014

--- python/0014-longest-common-prefix.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ""
-        for s in list(zip(*strs)):
-            if len(set(s)) == 1:
-                res += s[0]
-            else:
-                break
-        return res
