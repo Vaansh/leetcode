02c945b6334b1e9394eb31931f7668a9c8014b77
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-26 04:17:32 -0400

    python: deleted solution for problem 2011

--- python/2011-final-value-of-variable-after-performing-operations.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def finalValueAfterOperations(self, operations: List[str]) -> int:
-        res = 0
-        for o in operations:
-            if "++" in o:
-                res += 1
-            elif "--" in o:
-                res -= 1
-        return res
