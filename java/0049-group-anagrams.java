af466cc0599f91737997350868a911eb398d0b86
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-25 20:00:25 -0400

    java: added solution for problem 0049

---
+++ java/0049-group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] cs = s.toCharArray();
+            Arrays.sort(cs);
+
+            String anagram = new String(cs);
+            List<String> val = map.getOrDefault(anagram, new ArrayList<String>());
+            val.add(s);
+            map.put(anagram, val);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
