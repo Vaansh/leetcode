88207bb9d4cae621a508f7b1b6f08d2dcdcf5191
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2023-08-26 04:17:32 -0400

    python: deleted solution for problem 1822

--- python/1822-sign-of-the-product-of-an-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def arraySign(self, nums: List[int]) -> int:
-        res = 1
-        for n in nums:
-            if n == 0:
-                return 0
-            res *= n
-        return abs(res) // res
